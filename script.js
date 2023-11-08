8174bb52c0ea98438be50182490a7b4a23a1257b
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-08 12:41:13 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-
+//your JS code here. If required.
+ const codes = document.querySelectorAll('.code');
+      for (let i = 0; i < codes.length; i++) {
+        codes[i].addEventListener('input', function() {
+          if (this.value.length === this.maxLength) {
+            codes[i + 1].focus();
+          }
+        });
+        codes[i].addEventListener('keydown', function(e) {
+          if (e.key === 'Backspace' && this.value.length === 0) {
+            codes[i - 1].focus();
+          }
+        });
+      }
     
