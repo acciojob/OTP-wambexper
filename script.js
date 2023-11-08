7b05109d25f84e8e7f39e9c6c30a7325c049a209
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-08 12:37:09 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
 
-    //your JS code here. If required.
-const codeInputs = document.querySelectorAll('.code');
-
-codeInputs.forEach((input, index) => {
-    input.addEventListener('input', (event) => {
-        if (event.data !== null) {
-            if (index < codeInputs.length - 1) {
-                codeInputs[index + 1].focus();
-            }
-        }
-    });
-
-    input.addEventListener('keydown', (event) => {
-        if (event.key === 'Backspace' && index > 0) {
-            codeInputs[index - 1].focus();
-            codeInputs[index].value = '';
-        }
-    });
-});
+    
